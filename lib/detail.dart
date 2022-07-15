e9d81575d8033409c2aa54f4194298561ecfa61d
Youssef <afifiyoussef99@gmail.com>
2022-07-15 18:32:54 +0100

    commit

--- lib/detail.dart
+++ lib/detail.dart
@@ -193,10 +193,30 @@ class _detailState extends State<detail> {
             ),
             Stack(
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 30.0),
-                  child: Text(
-                      'This sample shows how to display a Divider between an orange and blue box inside a column. The Divider is 20 logical pixels in height and contains a vertically centered black line that is 5 logical pixels thick. The black line is indented by 20 logical pixels.'),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical, //.horizontal
+                    child: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                        children: [
+                          Text(
+                            "1 Description that is too long in text format(Here Data is coming from API) jdlksaf j klkjjflkdsjfkddfdfsdfds " +
+                                "2 Description that is too long in text format(Here Data is coming from API) d fsdfdsfsdfd dfdsfdsf sdfdsfsd d " +
+                                "3 Description that is too long in text format(Here Data is coming from API)  adfsfdsfdfsdfdsf   dsf dfd fds fs" +
+                                "4 Description that is too long in text format(Here Data is coming from API) dsaf dsafdfdfsd dfdsfsda fdas dsad" +
+                                "5 Description that is too long in text format(Here Data is coming from API) dsfdsfd fdsfds fds fdsf dsfds fds " +
+                                "6 Description that is too long in text format(Here Data is coming from API) asdfsdfdsf fsdf sdfsdfdsf sd dfdsf" +
+                                "7 Description that is too long in text format(Here Data is coming from API) df dsfdsfdsfdsfds df dsfds fds fsd" +
+                                "8 Description that is too long in text format(Here Data is coming from API)" +
+                                "9 Description that is too long in text format(Here Data is coming from API)" +
+                                "10 Description that is too long in text format(Here Data is coming from API)",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
                 Padding(
                   padding: EdgeInsets.only(
