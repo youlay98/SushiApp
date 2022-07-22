b150b8ca51824c355c3436301a587efacd59a630
Youssef <afifiyoussef99@gmail.com>
2022-07-22 17:01:08 +0100

    updating

---
+++ lib/textstyle.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const largTextSize = 26.0;
+const meduemTextSize = 20.0;
+const bodyTextSize = 16.0;
+
+const String fontfamily = 'lat';
+
+// ignore: constant_identifier_names
+const TitleTextStyle = TextStyle(
+    fontFamily: fontfamily,
+    fontWeight: FontWeight.w300,
+    fontSize: largTextSize,
+    color: Colors.black);
+
+const bodyTextStyle = TextStyle(
+    fontFamily: fontfamily,
+    fontWeight: FontWeight.w300,
+    fontSize: meduemTextSize,
+    color: Colors.black);
